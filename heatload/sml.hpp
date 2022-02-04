acc167428d7bde35e828f13f890b692673f15814
Seung-Hoe Ku <sku@pppl.gov>
2022-02-04 10:45:30 -0500

    add sml.hpp

---
+++ heatload/sml.hpp
@@ -0,0 +1,20 @@
+#ifndef SML_HPP
+#define SML_HPP
+
+// Simulation parameters
+class Simulation {
+    public:
+
+    // "Null constructor"
+
+    // Input parameters
+    double psix;
+
+    Simulation();
+};
+
+inline Simulation::Simulation() {
+
+};
+
+#endif
