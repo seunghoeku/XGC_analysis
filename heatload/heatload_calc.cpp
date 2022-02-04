bd2e08adae1a931506540dd02ddb4031ded08a7c
Seung-Hoe Ku <sku@pppl.gov>
2022-02-03 21:48:31 -0500

    Separate load and calculation routine from main.cpp

---
+++ heatload/heatload_calc.cpp
@@ -0,0 +1,2 @@
+
+void heatload_calc() {}
--- heatload/main.cpp
+++ heatload/main.cpp
@@ -85,34 +85,4 @@ void heatload() {
 
 void init() {}
 
-void load_data(adios2::IO reader_io, adios2::Engine reader,
-               std::vector<long> &igid, std::vector<float> &iphase,
-               std::vector<long> &egid, std::vector<float> &ephase) {
-  // Clear vector
-  igid.clear();
-  egid.clear();
-  iphase.clear();
-  ephase.clear();
-
-  // Inquire variables
-  auto var_igid = reader_io.InquireVariable<long>("igid");
-  auto var_egid = reader_io.InquireVariable<long>("egid");
-  auto var_iphase = reader_io.InquireVariable<float>("iphase");
-  auto var_ephase = reader_io.InquireVariable<float>("ephase");
-
-  var_igid.SetSelection({{0}, {var_igid.Shape()[0]}});
-  var_egid.SetSelection({{0}, {var_egid.Shape()[0]}});
-  var_iphase.SetSelection(
-      {{0, 0}, {var_iphase.Shape()[0], var_iphase.Shape()[1]}});
-  var_ephase.SetSelection(
-      {{0, 0}, {var_ephase.Shape()[0], var_ephase.Shape()[1]}});
-
-  reader.Get<long>(var_igid, igid);
-  reader.Get<long>(var_egid, egid);
-  reader.Get<float>(var_iphase, iphase);
-  reader.Get<float>(var_ephase, ephase);
-}
-
-void heatload_calc() {}
-
 void output() {}
