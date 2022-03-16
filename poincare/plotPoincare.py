d54fdb9f15af5548c27ab7883e3bc3c082d14762
Dave Pugmire <dpugmire@gmail.com>
2022-03-16 10:21:33 -0400

    Add plotting script.

---
+++ poincare/plotPoincare.py
@@ -0,0 +1,48 @@
+import adios2
+import sys
+import matplotlib.pyplot as plt
+import matplotlib.tri as tri
+
+print(sys.argv)
+print(sys.argv[1])
+
+if len(sys.argv) != 4 :
+    print('usage: ', sys.argv[0], ' infile outfile skip')
+    sys.exit()
+
+
+inFile = sys.argv[1]
+outFile = sys.argv[2]
+skip = int(sys.argv[3])
+
+f=adios2.open(inFile, 'r')
+tp=f.read('ThetaPsi')
+ID=f.read('ID')
+TS=f.read('TimeStep')
+
+n = int(len(tp) / 2)
+
+fig = plt.figure(figsize=[8,12])
+id0 = -1
+x = []
+y = []
+
+cnt = 0
+for i in range(0, n, skip) :
+    id = ID[i]
+    if id != id0 and len(x) > 0 :
+        plt.scatter(x, y, s=1, marker='x', edgecolor='none')
+        x = []
+        y = []
+        id0 = id
+        cnt = cnt+1
+
+    x.append(tp[i*2+0])
+    y.append(tp[i*2+1])
+
+plt.scatter(x, y, s=1, marker='x', edgecolor='none')
+print('num field lines= %d' % cnt)
+
+plt.title('%s ts=%d' % (inFile, TS[0]))
+plt.savefig(outFile + '.' + str(TS[0]) + '.png', format='png')
+#plt.show()
